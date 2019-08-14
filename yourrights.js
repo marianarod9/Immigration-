4d3579caac61407ae100e08ba5db31014006841e
marianarod9 <53275762+marianarod9@users.noreply.github.com>
2019-08-14 13:28:01 -0400

    Add files via upload

---
+++ yourrights.js
@@ -0,0 +1,15 @@
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "500px";
+  document.getElementById("main").style.marginLeft = "250px";
+
+
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+
+}
